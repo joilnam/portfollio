2e5abf90d1af8303cf0c5688aaf2f8ba577a056b
joilnam <79505221+joilnam@users.noreply.github.com>
2021-07-18 18:10:19 +0900

    js-arrow up

--- main.js
+++ main.js
@@ -37,8 +37,20 @@ document.addEventListener('scroll',()=>{
    home.style.opacity = 1-window.scrollY/homeHeight;
 });
 
+//show "arrow up" btn when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll',()=>{
+    if(window.scrollY>homeHeight/2){
+        arrowUp.classList.add('visible');
+    }else{
+        arrowUp.classList.remove('visible');
+    }
+})
 
-
+//Handle click on the 'arrow up' btn
+arrowUp.addEventListener('click',()=>{
+    scrollIntoView('#home');
+});
 
 
 
