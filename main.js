9a4705b9d98f7553c47fde8ef6f9c5eceb2d9c7c
joilnam <79505221+joilnam@users.noreply.github.com>
2021-07-18 23:40:41 +0900

    js

--- main.js
+++ main.js
@@ -52,6 +52,27 @@ arrowUp.addEventListener('click',()=>{
     scrollIntoView('#home');
 });
 
+//Project
+
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+
+workBtnContainer.addEventListener('click',(e)=>{
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset(filter);
+    if(filter==null){
+        return;
+    }
+
+    projects.forEach((project)=>{
+        if(filter==='*'||filter===project.dataset.type){
+            project.classList.remove('invisible');
+        }else{
+            project.classList.add('invisible');
+        }
+    });
+});
+
 
 
 
